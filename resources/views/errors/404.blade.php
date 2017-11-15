bc8478fe764657cb46cec90188c810704b9fa0df
Unknown <clement.lesage@viacesi.fr>
2017-11-15 19:02:24 +0100

    Modified 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,4 +9,16 @@
 @endsection
 
 @section('content')
+    <div class="content">
+        <div class="row">
+            <div class="col-sm-12" align="center">
+                <h2>Ne t'en fais pas la page que tu recherche n'est pas loin</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12" align="center">
+                <img src="{{ asset('site-img/dog-anim.gif') }}" >
+            </div>
+        </div>
+    </div>
 @endsection
