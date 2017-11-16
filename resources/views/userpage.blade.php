5d98ccf6b2f339daaf5ca871c3c95b020cd5ff4b
Unknown <clement.lesage@viacesi.fr>
2017-11-16 10:46:51 +0100

    edited

--- resources/views/userpage.blade.php
+++ resources/views/userpage.blade.php
@@ -47,7 +47,7 @@
                     @if($users->accept == 1)
                         <img src="{{ asset('site-img/Logo.png') }}" alt="logo" style="width: 128px">
                     @else
-                        <img src="{{ asset('site-img/logo-groupeCesi.png') }}" alt="logo" style="width: 128px">
+                        <img src="{{ asset('site-img/nouveau-logo-cesi2.jpg') }}" alt="logo" style="width: 128px; border-radius: 50%; height: 128px;">
                     @endif
                 @else
                     <img src="{{ asset('site-img/Logo.png') }}" alt="logo" style="width: 128px">
@@ -71,7 +71,7 @@
                     @if($users->accept == 1)
                         <img src="{{ asset('site-img/Logo.png') }}" alt="logo" style="width: 128px">
                     @else
-                        <img src="{{ asset('site-img/nouveau-logo-cesi2.jpg') }}" alt="logo" style="width: 128px; border-radius: 50%;">
+                        <img src="{{ asset('site-img/nouveau-logo-cesi2.jpg') }}" alt="logo" style="width: 128px; border-radius: 50%; height: 128px;">
                     @endif
                 @else
                     <img src="{{ asset('site-img/Logo.png') }}" alt="logo" style="width: 128px">
