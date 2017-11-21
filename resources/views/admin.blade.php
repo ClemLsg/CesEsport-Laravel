f81828bb34fa5feb30a3319aa8d429908f64d45e
Unknown <clement.lesage@viacesi.fr>
2017-11-21 13:47:18 +0100

    fix

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -21,23 +21,26 @@
     <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
     <div class="container">
         <div class="row">
-            <div class="col-sm-3">
+            <div class="col-sm-2">
                 <div class="nav-side-menu">
                     <div class="brand">Brand Logo</div>
                     <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
                     <div class="menu-list">
                         <ul id="menu-content" class="menu-content collapse out">
-                            <li>
-                                <a href="#">
-                                    <i class="fa fa-dashboard fa-lg"></i> Dashboard
-                                </a>
-                            </li>
-                            <li data-toggle="collapse" data-target="#new" class="collapsed">
-                                <a href="#"><i class="fa fa-car fa-lg"></i> New <span class="arrow"></span></a>
-                            </li>
-                            <ul class="sub-menu collapse" id="new">
-                                <li>New New 1</li>
-                            </ul>
+                            <div class="row">
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-dashboard fa-lg"></i> Dashboard
+                                    </a>
+                                </li>
+                            </div>
+                            <div class="row">
+                                <li>
+                                    <a href="#">
+                                        <i class="fa fa-dashboard fa-lg"></i> Dashboard
+                                    </a>
+                                </li>
+                            </div>
                         </ul>
                     </div>
                 </div>
