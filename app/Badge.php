531fc05dcd73f8626f9bbee9c840e3e3a4b02adf
Unknown <clement.lesage@viacesi.fr>
2018-01-02 16:54:14 +0100

    fix badge

--- app/Badge.php
+++ app/Badge.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Badge extends Model
 {
+    protected $fillable = [
+        'name',
+        'desc'
+    ];
     public function users()
     {
         return $this->belongsToMany('CesEsport\User')->as('user');
