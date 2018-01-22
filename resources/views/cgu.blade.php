c2c05b4ce92d44e6de74d404294ae5bfad4b0f15
Unknown <clement.lesage@viacesi.fr>
2018-01-22 15:55:49 +0100

    modified cgu

--- resources/views/cgu.blade.php
+++ resources/views/cgu.blade.php
@@ -21,46 +21,44 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-8 offset-2">
-                <p>
-                    03/03/2018 - Houpeville
-                    Prix d’entrée : 10€
-
-                    ATTITUDE DU JOUEUR
-
-                    Pour que la LAN Party se passe au mieux, il est indispensable que tout se déroule dans une ambiance conviviale, c'est pourquoi :
-                    - il est interdit de tenir des propos racistes, injurieux ou de dénigrer les autres participants,
-                    - il est indispensable que chacun fasse preuve d'un minimum de fair-play pendant les épreuves. L'utilisation de cheats codes ou de programmes permettant de tricher ou d'augmenter la luminosité de l'écran de façon excessive est interdite.
-
-                    MATÉRIEL
-
-                    Chaque participant devra apporter :
-                    - un PC complet (unité centrale munie d'une carte réseau 10/100 ou 10/100/1000 Ethernet, clavier, souris, écran, casque audio, câble d'alimentation),
-                    - une prise multiple longue (pas de "triplettes"),
-                    - un câble RJ-45 droit de cinq mètres au minimum (c'est encore mieux s'il est blindé),
-                    - ses propres jeux (originaux non piratés). Les patchs seront disponibles sur place,
-                    - chaque PC doit avoir un anti-virus à jour.
-                    Une IP fixe vous sera fournie à votre arrivée. Vous devrez garder la même tout au long de la LANParty. Chaque participant est responsable de son matériel. L'association Ces’ESport n'est pas responsable en cas de dégradation ou de vol.
-
-                    INTERDICTIONS
-
-                    Pendant toute la durée de l'événement il n'est pas autorisé :
-                    - de fumer dans la salle,
-                    - d'apporter d'autres appareils électriques que ceux qui sont définis dans le paragraphe ci-dessus. Les petits réfrigérateurs, ventilateurs et autres sont proscrits,
-                    - d'amener des réchauds à gaz et autres appareils de cuisine. Il est interdit de faire la cuisine dans la salle,
-                    - d'écouter la bande-son des jeux avec des enceintes. Pour la tranquillité de tous, seuls les casques sont autorisés,
-                    - de tenter de prendre le contrôle via le réseau d'un autre PC ou d'un serveur.
-
-                    Mot de passe pour l’inscription à la LAN : houpevillan
-
-                </p>
-                <b>
-                    Toute personne ne respectant pas les règles mentionnées dans ce règlement sera immédiatement exclue sans remboursement.
-                </b>
+                <p>03/03/2018 - Houpeville</p>
+                <p>Prix d&rsquo;entr&eacute;e&nbsp;: 10&euro;</p>
+                <p>&nbsp;</p>
+                <p>ATTITUDE DU JOUEUR</p>
+                <p>&nbsp;</p>
+                <p>Pour que la&nbsp;LAN&nbsp;Party se passe au mieux, il est indispensable que tout se d&eacute;roule dans une ambiance conviviale, c'est pourquoi :</p>
+                <p>- il est interdit de tenir des propos racistes, injurieux ou de d&eacute;nigrer les autres participants,</p>
+                <p>- il est indispensable que chacun fasse preuve d'un minimum de fair-play pendant les &eacute;preuves. L'utilisation de&nbsp;cheats codes&nbsp;ou de programmes permettant de tricher ou d'augmenter la luminosit&eacute; de l'&eacute;cran de fa&ccedil;on excessive est interdite.</p>
+                <p>&nbsp;</p>
+                <p>MAT&Eacute;RIEL</p>
+                <p>&nbsp;</p>
+                <p>Chaque participant devra apporter :</p>
+                <p>- un PC complet (unit&eacute; centrale munie d'une carte r&eacute;seau 10/100 ou 10/100/1000 Ethernet, clavier, souris, &eacute;cran, casque audio, c&acirc;ble d'alimentation),</p>
+                <p>- une prise multiple longue (pas de "triplettes"),</p>
+                <p>- un c&acirc;ble RJ-45 droit de cinq m&egrave;tres au minimum (c'est encore mieux s'il est blind&eacute;),</p>
+                <p>- ses propres jeux (originaux non pirat&eacute;s). Les&nbsp;patchs&nbsp;seront disponibles sur place,</p>
+                <p>- chaque PC doit avoir un anti-virus &agrave; jour.</p>
+                <p>Une IP fixe vous sera fournie &agrave; votre arriv&eacute;e. Vous devrez garder la m&ecirc;me tout au long de la&nbsp;LANParty. Chaque participant est responsable de son mat&eacute;riel. L'association Ces&rsquo;ESport n'est pas responsable en cas de d&eacute;gradation ou de vol.</p>
+                <p>&nbsp;</p>
+                <p>INTERDICTIONS</p>
+                <p>&nbsp;</p>
+                <p>Pendant toute la dur&eacute;e de l'&eacute;v&eacute;nement il n'est pas autoris&eacute; :</p>
+                <p>- de fumer dans la salle,</p>
+                <p>- de consommer de l'alcool,</p>
+                <p>- d'apporter d'autres appareils &eacute;lectriques que ceux qui sont d&eacute;finis dans le paragraphe ci-dessus. Les petits r&eacute;frig&eacute;rateurs, ventilateurs et autres sont proscrits,</p>
+                <p>- d'amener des r&eacute;chauds &agrave; gaz et autres appareils de cuisine. Il est interdit de faire la cuisine dans la salle,</p>
+                <p>- d'&eacute;couter la bande-son des jeux avec des enceintes. Pour la tranquillit&eacute; de tous, seuls les casques sont autoris&eacute;s,</p>
+                <p>- de tenter de prendre le contr&ocirc;le via le r&eacute;seau d'un autre PC ou d'un serveur.</p>
+                <p>&nbsp;</p>
+                <p>Mot de passe pour l&rsquo;inscription &agrave; la LAN&nbsp;: houpevillan</p>
+                <p>&nbsp;</p>
+                <p><strong>Toute personne ne respectant pas les r&egrave;gles mentionn&eacute;es dans ce r&egrave;glement sera imm&eacute;diatement exclue sans remboursement.</strong></p>
+                <p>&nbsp;</p>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-8 offset-2">
-                <form class="form-inline" method="POST" enctype="multipart/form-data" action="/cgu">
+                <form class="form" method="POST" enctype="multipart/form-data" action="/cgu">
                     {{ csrf_field() }}
                     <div class="g-recaptcha" data-sitekey="6LcX8kEUAAAAADfQsvqlKYzbzAYVfwU2gTONK_p1"></div>
                     <div class="form-check">
@@ -69,7 +67,7 @@
                             Je confirme avoir pris connaissance des conditions generale d'utilisation
                         </label>
                     </div>
-                    <button type="submit" class="btn btn-primary">Continuer</button>
+                    <button type="submit" class="btn btn-custo-inverted">Continuer</button>
                 </form>
             </div>
         </div>
